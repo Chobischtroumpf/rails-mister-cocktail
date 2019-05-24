44ede084e9fa0a98bf1f8898fe0dde87170ec77d
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-05-24 14:34:29 +0200

    merged with clean code

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Your ingredients are being created.."
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'Coke')
+Ingredient.create(name: 'Sprite')
+Ingredient.create(name: 'Orange')
+Ingredient.create(name: 'Tomato juice')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+puts "Your ingredients are created!"
+
+
